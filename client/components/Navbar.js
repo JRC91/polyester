10c53653fbaa7a7ba271ec879e6490dc2fd59df2
JRC91 <jimcarmody91@gmail.com>
2022-07-28 17:25:15 -0400

    working on donate

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -1,17 +1,35 @@
 import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
-
+// const PayPalButton = paypal.Buttons.driver("react", { React, ReactDOM });
 import { Dropdown, Button } from "react-bootstrap";
 export function Navbar() {
   const [opaqueSetting, setOpaque] = useState(["0"]);
-
+  const [toggle, setToggle] = useState(false)
   function handleScroll(evt) {
 
     if (opaqueSetting === "0") {
       setOpaque("50");
     }
   }
+
+  function toggleClick () {
+    if(toggle){
+      setToggle(false)
+    }
+    if(toggle)
+    {setToggle(true)}
+  }
+
+
+
+
+
+
+
+
+
+
 
   return (
     <div>
@@ -37,22 +55,18 @@ export function Navbar() {
             <Link to="/about">
               <button className="nav-link">About Us</button>
             </Link>
+
+            <button  onClick={toggleClick}className="donate">Donate</button>
           </a>
 
-          {/* <Dropdown className='top-50px'>
-<Dropdown.Toggle  variant="dark" id="dropdown-basic">
-    Learn More
-  </Dropdown.Toggle>
-
-  <Dropdown.Menu>
-    <Dropdown.Item href="#/action-1">How To Help</Dropdown.Item>
-    <Dropdown.Item href="#/action-2">Resources</Dropdown.Item>
-    <Dropdown.Item href="#/action-3">About Us</Dropdown.Item>
-  </Dropdown.Menu>
-</Dropdown> */}
         </div>
       </nav>
+    {toggle?
+    <div>
 
+  {/* <PayPalButton></PayPalButton> */}
+  </div>
+    : <></>}
     </div>
   );
 }
